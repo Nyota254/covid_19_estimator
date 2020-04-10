73f87e45d39e9e8843d08ec82b1f302e9aaba26e
Nyota254 <knyota66@gmail.com>
2020-04-10 17:28:58 +0300

    completed challenge1 of SDG

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,73 @@
+
 def estimator(data):
+  '''
+  Method for impact estimation on the covid_19 pandemic based on calculations
+  and studys by Havard Medical School and Massachusetts General Hospital
+
+  Args:
+      Dictionary such as the example below.
+      {
+        region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+        },
+        periodType: "days",
+        timeToElapse: 58,
+        reportedCases: 674,
+        population: 66622705,
+        totalHospitalBeds: 1380614
+      }
+  '''
+  #original data storage
+  originalData = data
+
+  #Calculations for Currently infected
+
+  currentlyInfectedImpact = data['reportedCases'] * 10
+  currentlyInfectedSeverImpact = data['reportedCases'] * 50
+
+  
+  # Start of calculations for infections by requested time factoring in one will use
+  # days,weeks and months
+
+  if data['periodType'] is "weeks":
+    data['timeToElapse'] = data['timeToElapse'] * 7
+
+  elif data['periodType'] is "months":
+    data['timeToElapse'] = data['timeToElapse'] * 30
+
+  else:
+    pass
+
+  powerNumber = int(data['timeToElapse']/3)
+  infectionsByRequestedTimeImpact = int(currentlyInfectedImpact * (2**powerNumber))
+  infectionsByRequestedTimeSeverImpact = int(currentlyInfectedSeverImpact * (2**powerNumber))
+
+
+  # data to be returned inform of a dictionary
+  data = {'data':{'region': {
+                              'name': originalData['region']['name'],
+                              'avgAge': originalData['region']['avgAge'],
+                              'avgDailyIncomeInUSD': originalData['region']['avgDailyIncomeInUSD'],
+                              'avgDailyIncomePopulation': originalData['region']['avgDailyIncomePopulation']
+                            },
+                  'periodType': originalData['periodType'],
+                  'timeToElapse': originalData['timeToElapse'],
+                  'reportedCases': originalData['reportedCases'],
+                  'population': originalData['population'],
+                  'totalHospitalBeds': originalData['totalHospitalBeds']
+          },
+          'estimate':{
+                        'impact':{'currentlyInfected': currentlyInfectedImpact,
+                                'infectionsByRequestedTime': infectionsByRequestedTimeImpact,
+                        },
+
+                        'severImpact':{'currentlyInfected':currentlyInfectedSeverImpact,
+                                      'infectionsByRequestedTime': infectionsByRequestedTimeSeverImpact,
+                        }
+          }
+  }
+
   return data
