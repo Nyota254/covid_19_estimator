d2545930b6fd45e0e0ac88e31a572422d12028b1
Nyota254 <knyota66@gmail.com>
2020-04-11 12:37:03 +0300

    Remove test data

--- src/estimator.py
+++ src/estimator.py
@@ -1,19 +1,5 @@
 from math import trunc
 
-data = {
-        'region': {
-        'name': "Africa",
-        'avgAge': 19.7,
-        'avgDailyIncomeInUSD': 4,
-        'avgDailyIncomePopulation': 0.73
-        },
-        'periodType': "days",
-        'timeToElapse': 38,
-        'reportedCases': 2747,
-        'population': 92931687,
-        'totalHospitalBeds': 678874
-      }
-
 def estimator(data):
   '''
   Method for impact estimation on the covid_19 pandemic based on calculations
@@ -122,5 +108,3 @@ def estimator(data):
   }
 
   return data
-
-print(estimator(data))
