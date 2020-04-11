0a674accb61f76bd67182aa08c8ccb1c798604a0
Nyota254 <knyota66@gmail.com>
2020-04-11 14:58:11 +0300

    remove testing data

--- src/estimator.py
+++ src/estimator.py
@@ -1,18 +1,4 @@
 
-data =     {
-        'region': {
-        'name': "Africa",
-        'avgAge': 19.7,
-        'avgDailyIncomeInUSD': 4,
-        'avgDailyIncomePopulation': 0.73
-        },
-        'periodType': "days",
-        'timeToElapse': 38,
-        'reportedCases': 2747,
-        'population': 92931687,
-        'totalHospitalBeds': 678874
-      }
-
 def estimator(data):
   '''
   Method for impact estimation on the covid_19 pandemic based on calculations
@@ -118,5 +104,3 @@ def estimator(data):
   }
 
   return data
-
-print(estimator(data))
